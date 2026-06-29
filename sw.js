24bc0d28cc79261c35c8d17a358c003c79bc21ad
OsmanWorks <osman.chaajao@gmail.com>
2026-06-29 16:28:22 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Bills Tracker Service Worker
+const CACHE = 'bills-tracker-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Don't cache Google Sheets requests
+  if (e.request.url.includes('docs.google.com') || e.request.url.includes('allorigins')) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
